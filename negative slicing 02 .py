3ccaa2e3291007869090c20c6d6280c63dd14fa6
its-mohan79 <mohanroy8989@gmail.com>
2025-12-23 22:39:12 +0530

    practice new code nagative slicing

---
+++ negative slicing 02 .py
@@ -0,0 +1,14 @@
+name ="mohan"
+
+print(name [0:4])
+
+
+print(name[-4:-1]) # it realated to each other 
+print(name[1:4])   # solve by using corresponding
+
+print(name[:4]) # it means same as [0:4]
+
+
+print(name[1:]) # it means same as [1:5]
+
+print(name[1:5])
---
+++ string intro 01.py
@@ -0,0 +1,8 @@
+name ="mohan"
+
+nameshort=name[0:4]    #its means {0:4} start at 1 to 4 but only 3 value for example moha is only given outputvalue
+
+character1=name[0]
+
+print(character1)
+print(nameshort)
